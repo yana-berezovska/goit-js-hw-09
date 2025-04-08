bfe39a2e10b24fa74464ebb6313c4df3404811e5
Yana <yanberezovska.w@gmail.com>
2025-04-08 21:33:41 +0300

    hw09

---
+++ src/js/2-form.js
@@ -0,0 +1,40 @@
+const formData = {
+  email: '',
+  message: '',
+};
+
+const localStorageKey = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+
+const savedForm = localStorage.getItem(localStorageKey);
+if (savedForm !== null) {
+  const parsedForm = JSON.parse(savedForm);
+  form.elements.message.value = parsedForm.message ?? '';
+  form.elements.email.value = parsedForm.email ?? '';
+  formData.email = parsedForm.email;
+  formData.message = parsedForm.message;
+}
+
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+
+  if (name === 'email' || name === 'message') {
+    formData[name] = value.trim();
+    localStorage.setItem(localStorageKey, JSON.stringify(formData));
+  }
+});
+
+form.addEventListener('submit', evnt => {
+  evnt.preventDefault();
+  const elements = evnt.target.elements;
+
+  if (elements.email.value === '' || elements.message.value === '') {
+    alert('Fill please all fields');
+  } else {
+    console.log(formData);
+    localStorage.removeItem(localStorageKey);
+    form.reset();
+    formData.email = '';
+    formData.message = '';
+  }
+});
